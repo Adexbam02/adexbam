e6363aebdb9c70232ea5a8151dccebff1ca79f65
Nurain <nurainbamidelejnr@gmail.com>
2024-10-29 14:10:56 +0100

    process

--- src/components/Process.tsx
+++ src/components/Process.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import process from "../ui/data";
 
 import doubleStar from "../../public/svgs/doubleStar.svg";
-import processOne from "../../public/processOne.png";
+// import processOne from "../../public/processOne.png";
 import Link from "next/link";
 export const Process = () => {
   return (
@@ -12,13 +13,43 @@ export const Process = () => {
       </div>
 
       <div className="content">
-        <div>
-          <div className="flex items-start justify-between gap-4">
+        <div className="flex flex-col items-center justify-center gap-[2rem]">
+          {process.map(({ id, number, img, title, content, link }) => (
+            <div
+              key={id}
+              className="flex items-start justify-between gap-4 bg-rd-500"
+            >
+              <div className="flex flex-col items-center justify-center gap-2 bgblue-200">
+                <span className="text-[15px] border-[1px] border-white rounded-full w-[35px] h-[35px] flex items-center justify-center">
+                  {number}
+                </span>
+                <span className="h-[170px] w-[0.5px] bg-white"></span>
+              </div>
+
+              <div className="flex flex-col items-start justify-center gap-4 bgred-500">
+                <Image src={img} width={30} height={30} alt="" />
+
+                <h3 className="text-[20px]">{title}</h3>
+
+                <p className="min-w-[200px]">
+                  {content}
+                </p>
+
+                <Link
+                  className="px-[15px] py-[7px] rounded-[25px] border-[0.4px] border-white"
+                  href={link}
+                >
+                  See Examples
+                </Link>
+              </div>
+            </div>
+          ))}
+          {/* <div className="flex items-start justify-between gap-4 bg-red-500">
             <div className="flex flex-col items-center justify-center gap-2 bgblue-200">
               <span className="text-[15px] border-[1px] border-white rounded-full w-[35px] h-[35px] flex items-center justify-center">
                 1
               </span>
-              <span className="h-[200px] w-[0.5px] bg-white"></span>
+              <span className="h-[170px] w-[0.5px] bg-white"></span>
             </div>
 
             <div className="flex flex-col items-start justify-center gap-4 bgred-500">
@@ -31,9 +62,9 @@ export const Process = () => {
                 of the market landscape, user needs, and existing solutions.
               </p>
 
-              <Link href={"/"}>See Examples</Link>
+              <Link className="px-[15px] py-[7px] rounded-[25px] border-[0.4px] border-white" href={"/"}>See Examples</Link>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
---
+++ src/ui/data.ts
@@ -0,0 +1,33 @@
+import processOne from "../../public/processOne.png";
+
+const process = [
+  {
+    id: 1,
+    number: 1,
+    img: processOne,
+    title: "Step 1: Product design Research",
+    content:
+      "This initial step involves a systematic and thorough exploration of the market landscape, user needs, and existing solutions.",
+    link: "/",
+  },
+  {
+    id: 2,
+    number: 2,
+    img: processOne,
+    title: "Step 1: Product design Research",
+    content:
+      "This initial step involves a systematic and thorough exploration of the market landscape, user needs, and existing solutions.",
+    link: "/",
+  },
+  {
+    id: 3,
+    number: 3,
+    img: processOne,
+    title: "Step 1: Product design Research",
+    content:
+      "This initial step involves a systematic and thorough exploration of the market landscape, user needs, and existing solutions.",
+    link: "/",
+  },
+];
+
+export default process;
