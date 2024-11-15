98ad364cf0e09b5543ef537882cc6757d3aa0502
Nurain <nurainbamidelejnr@gmail.com>
2024-11-15 15:54:59 +0100

    fixed Project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -10,7 +10,7 @@ import { useEffect } from "react";
 
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { useGSAP } from "@gsap/react";
+// import { useGSAP } from "@gsap/react";
 
 gsap.registerPlugin(ScrollTrigger);
 
