0b9f187bc8e7664f75a72365dc7fa46c639b3be0
Nurain <nurainbamidelejnr@gmail.com>
2024-11-15 15:56:03 +0100

    fixes Social

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -6,30 +6,11 @@ import dribble from "../../public/svgs/dribble.svg";
 import insta from "../../public/svgs/instagram.svg";
 import facebook from "../../public/svgs/facebook.svg";
 // import { social } from "@/ui/data"
-import adexbam from "../../public/adexbam.png";
 
-import gsap from "gsap";
-import { useGSAP } from "@gsap/react";
+// import gsap from "gsap";
+// import { useGSAP } from "@gsap/react";
 export const Social = () => {
-  const mouseHoverIn = () => {
-    console.log("IN");
-
-    const timeline = gsap.timeline();
-
-    timeline.fromTo(
-      "#dexbam",
-      { opacity: 0, x: 0 },
-      { opacity: 1, x: 10, duration: 0.7, delay: 0.0 }
-    );
-  };
-
-  const mouseHoverOut = () => {
-    // console.log("OUT");
-
-    const timeline = gsap.timeline();
-
-    timeline.to("#dexbam", { opacity: 0, x: -15, duration: 0.7 });
-  };
+  
 
   return (
     <div className="flex items-center justify-center w-[100%] px-[23px] mt-[5rem] p-4 ">
