736eeae1e9f1a9c068f9c0f351115e6f99807693
Nurain <nurainbamidelejnr@gmail.com>
2024-11-16 15:39:23 +0100

    add

---
+++ src/app/about/page.tsx
@@ -0,0 +1,39 @@
+export default function Page() {
+  return (
+    <header className="mt-[1rem] px-[23px] flex items-center justfy-center flex-col md:px-[60px]  lg:px-[9rem]">
+      <h1 className="text-[70px] font-bold text-center">About Me</h1>
+
+      <p className="mb-4 text-cener">
+        I am a Website Developer, I create captivating content that visually
+        articulates the brand&apos;s ideas and goals, specializing in sports
+        designs for social media platforms. With the help of Adobe
+      </p>
+
+      <p className="mb-4 text-cener">
+        Creative Suite, motion graphics, and video editing, I convey emotions,
+        evoke feelings, and narrate stories through the art form. I have won
+        multiple community awards for my outstanding sports designs, such as
+        posters, logos, flyers, and banners, that have generated high engagement
+        and positive feedback from the audience.
+      </p>
+
+      <p className="mb-4 text-cener">
+        I am also pursuing a Bachelor&apos;s degree in Agricultural and
+        Environmental Engineering from the University of Ibadan, which will
+        enable me to apply my technical skills and knowledge to my design
+        projects.
+      </p>
+
+      <p className="mb-4 text-cener">
+        I am passionate about photography and appreciate the intricacies and
+        aesthetics of the medium. I collaborate with clients and colleagues to
+        address visual challenges and deliver high-quality results. I constantly
+        seek to learn new tools and techniques to enhance my creativity and
+        productivity. My goal is to become a versatile and innovative designer
+        who can make an impact in the sports industry. I aim to become a
+        versatile and innovative designer who can impact in the sport design
+        industry
+      </p>
+    </header>
+  );
+}
