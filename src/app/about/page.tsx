c4cf083a762d2bb3f58ac709ab004bac02335f37
Nurain <nurainbamidelejnr@gmail.com>
2024-11-17 07:00:00 +0100

    add

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,23 +3,25 @@ export default function Page() {
     <header className="ctaBg mt-[1rem] px-[23px] flex items-center justfy-center flex-col md:px-[60px]  lg:px-[9rem]">
       <h1 className="text-[70px] font-bold text-center">About Me</h1>
 
-      <div className="relative flex items-start h[100%]">
-        <div className="flex gap-[4rem] w-[100px] bg-blue-400 flex-col h-[200px] wfit">
-        {/* <p className="bg-black w-[100%] rotate-90 text-[10px]">REASEARCH <br /> DEVELOPMENT DESIGNING</p> */}
-        <p className="rotate-90 text-[10px] ">REASEARCH</p>
-        <p className="rotate-90 text-[10px]">DEVELOPMENT</p>
-        <p className="rotate-90 text-[10px]">DESIGNING</p>
-        </div>
-        
+      <div className="grid grid-cols-[7%_93%] items-start gap-[0rem] w-[100%]">
+        {/* <p className="bg-red-400 w-[100%] h-fit rotate-text text-[10px]">
+          REASEARCH DEVELOPMENT DESIGNING
+        </p> */}
 
-        <div className="bg-red-600">
-          <p className="mb-4 text-cener">
+        <div className="bg-bue-300 w-[30px] h-[100vh] flex items-start justify-start">
+          <p className="b-red-400 w-[100%] h-fit rotate-text text-[10px]">
+            REASEARCH DEVELOPMENT DESIGNING
+          </p>
+        </div>
+
+        <div className="b-red-600 w-[100%]">
+          <p className="mb-4 text-left">
             I am a Website Developer, I create captivating content that visually
             articulates the brand&apos;s ideas and goals, specializing in sports
             designs for social media platforms. With the help of Adobe
           </p>
 
-          <p className="mb-4 text-cener">
+          <p className="mb-4 text-left">
             Creative Suite, motion graphics, and video editing, I convey
             emotions, evoke feelings, and narrate stories through the art form.
             I have won multiple community awards for my outstanding sports
@@ -27,14 +29,14 @@ export default function Page() {
             generated high engagement and positive feedback from the audience.
           </p>
 
-          <p className="mb-4 text-cener">
+          <p className="mb-4 text-left">
             I am also pursuing a Bachelor&apos;s degree in Agricultural and
             Environmental Engineering from the University of Ibadan, which will
             enable me to apply my technical skills and knowledge to my design
             projects.
           </p>
 
-          <p className="mb-4 text-cener">
+          <p className="mb-4 text-left">
             I am passionate about photography and appreciate the intricacies and
             aesthetics of the medium. I collaborate with clients and colleagues
             to address visual challenges and deliver high-quality results. I
