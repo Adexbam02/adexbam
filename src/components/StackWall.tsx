e397da4c15e5610825229eff79cf1a0d6e39fce1
Nurain <nurainbamidelejnr@gmail.com>
2024-11-21 11:13:28 +0100

    add.

--- src/components/StackWall.tsx
+++ src/components/StackWall.tsx
@@ -3,19 +3,18 @@ import stackk from "../../public/svgs/stacks/stack";
 
 export const StackWall = () => {
   return (
-    <div className="px-[22.5px] py-[35px] relative border-white/20 border-t-[0px] border-b-[1px] md:border-x-[1px] h-fit w-[100%] bg-transparent flex items-center justify-center gap-3 flex-wrap">
-    {
-      stackk.map(({id, img}) => (
-        <Image
-        key={id}
-        src={img}
-        width={35}
-        height={25}
-        alt="star"
-        className="md:grayscale cursor-pointer transition-all hover:grayscale-0 sm:w-[40px]"
-      />
-      ))
-    }
+    <div className="px-[22.5px] py-[35px] relative border-white/20 border-t-[0px] border-b-[1px] md:border-x-[1px] h-fit w-[100%] bg-transparent grid grid-cols-4 gap-x-[5px] gap-y-[5px]">
+      {stackk.map(({ id, img }) => (
+        <div key={id} className="w-[100px] h-[100px]  rounded-[3px] bg-[#202127] grid place-items-center">
+          <Image
+            src={img}
+            width={30}
+            height={20}
+            alt="star"
+            className=" p- md:grayscale cursor-pointer transition-all hover:grayscale-0 sm:w-[40px]"
+          />
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
